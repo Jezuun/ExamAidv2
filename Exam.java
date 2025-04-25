8f93f7b9cf0eac043c5c78da58e229121cbabbaf
Jesse <Jesseolivermd@gmail.com>
2025-04-25 08:57:56 -0400

    First version, no edits made

---
+++ Exam.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class Exam here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Exam
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Exam
+     */
+    public Exam()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod(int y)
+    {
+        // Q24
+        // String a= new String;
+        // String b = new String;
+        //System.out.println(if(a==b;"yes":"no));
+        
+    }
+    
+    public void maxTemp()
+    {
+        //Q25
+        //*Take ArrayList*
+        //if (Temp > 37,5; int i=0; i++);
+        //return int i;
+        
+    }
+    
+    public void tempLoop()
+    {
+        //q26
+    // same thing but you return the collections.Max
+    }
+    
+    public void termpSort()
+    {
+        //Q27
+        //Return the final index of the arrayList after calling collections.sort
+    }
+    }
+
